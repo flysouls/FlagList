fb8bcf10dc074377494803359588de5fd4b43418
unknown <P0110589@pacteragy.com>
2018-07-09 18:03:15 +0800

    update

--- public/src/js/pages/helloworld/app.vue
+++ public/src/js/pages/helloworld/app.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <h2>我的个人网站-</h2>
-    <h1>{{a}}</h1>
-    <h3>生活就像在黑屋子里洗衣服，你不知道洗干净没有，只能一遍一遍的洗。</h3>
-    <h3>直到那一天到来，你会发现认真洗过的衣服很干净，以后你每次穿都会感谢当初洗衣服的日子。</h3>
+    <canvas id="canvas"></canvas>
+    <h2>{{title}}</h2>
+    <h5>{{a}}</h5>
+    <h3>遍一遍的洗。&lt;br&gt;直到那</h3>
+    <h3 v-html='text'></h3>
   </div>
 </template>
 
@@ -13,7 +14,18 @@ export default {
   name: 'App',
   data: () => {
     return {
-      a:"male"
+      a: "对，你没看错，就是空白页面",
+      text: window.p_text,
+      title: window.p_title,
+      PR: window.devicePixelRatio || 1, // dpr
+      WIDTH: window.innerWidth,
+      HEIGHT: window.innerHeight,
+      F: 90,
+      q: [], // 俩个点（三角形的一条边）
+      r: 0, // 取色sin(r)*256
+      u: Math.PI*2,
+      v: Math.sin,
+      context: undefined
     }
   },
   watch:{
@@ -21,9 +33,52 @@ export default {
   computed: {
   },
   mounted(){
-    console.log("页面通了");
+    var canvas = document.getElementById('canvas');
+    this.context = canvas.getContext('2d');
+    canvas.width = this.WIDTH * this.PR;
+    canvas.height = this.HEIGHT * this.PR;
+    this.context.scale(this.PR, this.PR);
+    this.context.globalAlpha = 0.6;
+    document.onclick = this.i;
+    // document.ontouchstart = this.i;
+    this.i();
   },
   methods:{
+    i(){
+      this.r = this.random();
+      this.context.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+      //定义最左边俩个点的纵向跨度为2F
+      this.q=[{x: 0,y: this.HEIGHT * 0.7 + this.F},{x: 0,y: this.HEIGHT * 0.7 - this.F}];
+      while(this.q[1].x < this.WIDTH + this.F) this.draw(this.q[0], this.q[1]);
+    },
+    random(){
+      return Math.random();
+    },
+    y(p){
+      // z()*2-1 小于0的几率较大 => y将减小
+      var t = p + (this.random()*2 - 1.1) * this.F;
+      return (t > this.HEIGHT || t < 0) ? y(p) : t;
+    },
+    c2c(cn){
+      //将10进制转为16进制
+      return Math.floor(Math.abs(cn)).toString(16);
+    },
+    draw(i,j){
+        this.context.beginPath()
+        this.context.moveTo(i.x, i.y);
+        this.context.lineTo(j.x, j.y);
+        var k = j.x + (this.random()*2-0.25)*this.F,
+            n = this.random(j.y);
+        this.context.lineTo(k, n);
+        this.context.closePath();
+        this.r += 0.2;
+        // x.fillStyle = '#'+(v(r)*127+128<<16 | v(r+u/3)*127+128<<8 | v(r+u/3*2)*127+128).toString(16)
+        // 尤大原版获取颜色的方法...需要理解位运算(保证了每个色值最小为127).以下是简化版
+        this.context.fillStyle = `#${this.c2c(this.v(this.r)*256)}${this.c2c(this.v(this.r+this.u/4)*256)}${this.c2c(this.v(this.r+(this.u/2))*256)}`;
+        this.context.fill();
+        this.q[0] = this.q[1];
+        this.q[1] = {x:k,y:n};
+    }
   },
   components: {
   }
@@ -31,6 +86,15 @@ export default {
 </script>
 
 <style>
+  #canvas{
+    width:100%;
+    height:100%;
+    position:absolute;
+    top:0;
+    left:0;
+    z-index:0;
+    pointer-events:none;
+  }
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
