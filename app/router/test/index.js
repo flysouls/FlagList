d5450275ad86a6eb8984791effbfea219adbd6f2
unknown <P0110589@pacteragy.com>
2018-07-18 14:24:58 +0800

    feat firework

--- app/router/test/index.js
+++ app/router/test/index.js
@@ -1,5 +1,5 @@
 const router = require("koa-router")();
 router.get('/test', async(ctx, next) => {
-  await ctx.render("pages/test", {msg:'router注入'});
+  await ctx.render("pages/test", {msg:'Vuex'});
 })
 module.exports = router;
--- public/src/js/pages/firework/app.vue
+++ public/src/js/pages/firework/app.vue
@@ -67,139 +67,133 @@ export default {
     }
 
     // create firework
-    function Firework( sx, sy, tx, ty ) {
-      // actual coordinates
-      this.x = sx;
-      this.y = sy;
-      // starting coordinates
-      this.sx = sx;
-      this.sy = sy;
-      // target coordinates
-      this.tx = tx;
-      this.ty = ty;
-      // distance from starting point to target
-      this.distanceToTarget = calculateDistance( sx, sy, tx, ty );
-      this.distanceTraveled = 0;
-      // track the past coordinates of each firework to create a trail effect, increase the coordinate count to create more prominent trails
-      this.coordinates = [];
-      this.coordinateCount = 3;
-      // populate initial coordinate collection with the current coordinates
-      while( this.coordinateCount-- ) {
-        this.coordinates.push( [ this.x, this.y ] );
-      }
-      this.angle = Math.atan2( ty - sy, tx - sx );
-      this.speed = 2;
-      this.acceleration = 1.05;
-      this.brightness = random( 50, 70 );
-      // circle target indicator radius
-      this.targetRadius = 1;
-    }
-
-    // update firework
-    Firework.prototype.update = function( index ) {
-      // remove last item in coordinates array
-      this.coordinates.pop();
-      // add current coordinates to the start of the array
-      this.coordinates.unshift( [ this.x, this.y ] );
-      
-      // cycle the circle target indicator radius
-      if( this.targetRadius < 8 ) {
-        this.targetRadius += 0.3;
-      } else {
+    class Firework{
+      constructor( sx, sy, tx, ty ){
+        // actual coordinates
+        this.x = sx;
+        this.y = sy;
+        // starting coordinates
+        this.sx = sx;
+        this.sy = sy;
+        // target coordinates
+        this.tx = tx;
+        this.ty = ty;
+        // distance from starting point to target
+        this.distanceToTarget = calculateDistance( sx, sy, tx, ty );
+        this.distanceTraveled = 0;
+        // track the past coordinates of each firework to create a trail effect, increase the coordinate count to create more prominent trails
+        this.coordinates = [];
+        this.coordinateCount = 3;
+        // populate initial coordinate collection with the current coordinates
+        while( this.coordinateCount-- ) {
+          this.coordinates.push( [ this.x, this.y ] );
+        }
+        this.angle = Math.atan2( ty - sy, tx - sx );
+        this.speed = 2;
+        this.acceleration = 1.05;
+        this.brightness = random( 50, 70 );
+        // circle target indicator radius
         this.targetRadius = 1;
       }
-      
-      // speed up the firework
-      this.speed *= this.acceleration;
-      
-      // get the current velocities based on angle and speed
-      var vx = Math.cos( this.angle ) * this.speed,
-          vy = Math.sin( this.angle ) * this.speed;
-      // how far will the firework have traveled with velocities applied?
-      this.distanceTraveled = calculateDistance( this.sx, this.sy, this.x + vx, this.y + vy );
-      
-      // if the distance traveled, including velocities, is greater than the initial distance to the target, then the target has been reached
-      if( this.distanceTraveled >= this.distanceToTarget ) {
-        createParticles( this.tx, this.ty );
-        // remove the firework, use the index passed into the update function to determine which to remove
-        fireworks.splice( index, 1 );
-      } else {
-        // target not reached, keep traveling
-        this.x += vx;
-        this.y += vy;
+      update( index ) {
+        // remove last item in coordinates array
+        this.coordinates.pop();
+        // add current coordinates to the start of the array
+        this.coordinates.unshift( [ this.x, this.y ] );
+        
+        // cycle the circle target indicator radius
+        if( this.targetRadius < 8 ) {
+          this.targetRadius += 0.3;
+        } else {
+          this.targetRadius = 1;
+        }
+        
+        // speed up the firework
+        this.speed *= this.acceleration;
+        
+        // get the current velocities based on angle and speed
+        var vx = Math.cos( this.angle ) * this.speed,
+            vy = Math.sin( this.angle ) * this.speed;
+        // how far will the firework have traveled with velocities applied?
+        this.distanceTraveled = calculateDistance( this.sx, this.sy, this.x + vx, this.y + vy );
+        
+        // if the distance traveled, including velocities, is greater than the initial distance to the target, then the target has been reached
+        if( this.distanceTraveled >= this.distanceToTarget ) {
+          createParticles( this.tx, this.ty );
+          // remove the firework, use the index passed into the update function to determine which to remove
+          fireworks.splice( index, 1 );
+        } else {
+          // target not reached, keep traveling
+          this.x += vx;
+          this.y += vy;
+        }
+      }
+      draw = function() {
+        ctx.beginPath();
+        // move to the last tracked coordinate in the set, then draw a line to the current x and y
+        ctx.moveTo( this.coordinates[ this.coordinates.length - 1][ 0 ], this.coordinates[ this.coordinates.length - 1][ 1 ] );
+        ctx.lineTo( this.x, this.y );
+        ctx.strokeStyle = 'hsl(' + hue + ', 100%, ' + this.brightness + '%)';
+        ctx.stroke();
+        
+        ctx.beginPath();
+        // draw the target for this firework with a pulsing circle
+        ctx.arc( this.tx, this.ty, this.targetRadius, 0, Math.PI * 2 );
+        ctx.stroke();
       }
     }
-
-    // draw firework
-    Firework.prototype.draw = function() {
-      ctx.beginPath();
-      // move to the last tracked coordinate in the set, then draw a line to the current x and y
-      ctx.moveTo( this.coordinates[ this.coordinates.length - 1][ 0 ], this.coordinates[ this.coordinates.length - 1][ 1 ] );
-      ctx.lineTo( this.x, this.y );
-      ctx.strokeStyle = 'hsl(' + hue + ', 100%, ' + this.brightness + '%)';
-      ctx.stroke();
-      
-      ctx.beginPath();
-      // draw the target for this firework with a pulsing circle
-      ctx.arc( this.tx, this.ty, this.targetRadius, 0, Math.PI * 2 );
-      ctx.stroke();
-    }
-
     // create particle
-    function Particle( x, y ) {
-      this.x = x;
-      this.y = y;
-      // track the past coordinates of each particle to create a trail effect, increase the coordinate count to create more prominent trails
-      this.coordinates = [];
-      this.coordinateCount = 5;
-      while( this.coordinateCount-- ) {
-        this.coordinates.push( [ this.x, this.y ] );
+    class Particle{
+      constructor(x,y){
+        this.x = x;
+        this.y = y;
+        // track the past coordinates of each particle to create a trail effect, increase the coordinate count to create more prominent trails
+        this.coordinates = [];
+        this.coordinateCount = 5;
+        while( this.coordinateCount-- ) {
+          this.coordinates.push( [ this.x, this.y ] );
+        }
+        // set a random angle in all possible directions, in radians
+        this.angle = random( 0, Math.PI * 2 );
+        this.speed = random( 1, 10 );
+        // friction will slow the particle down
+        this.friction = 0.95;
+        // gravity will be applied and pull the particle down
+        this.gravity = 1;
+        // set the hue to a random number +-50 of the overall hue variable
+        this.hue = random( hue - 50, hue + 50 );
+        this.brightness = random( 50, 80 );
+        this.alpha = 1;
+        // set how fast the particle fades out
+        this.decay = random( 0.015, 0.03 );
       }
-      // set a random angle in all possible directions, in radians
-      this.angle = random( 0, Math.PI * 2 );
-      this.speed = random( 1, 10 );
-      // friction will slow the particle down
-      this.friction = 0.95;
-      // gravity will be applied and pull the particle down
-      this.gravity = 1;
-      // set the hue to a random number +-50 of the overall hue variable
-      this.hue = random( hue - 50, hue + 50 );
-      this.brightness = random( 50, 80 );
-      this.alpha = 1;
-      // set how fast the particle fades out
-      this.decay = random( 0.015, 0.03 );
-    }
-
-    // update particle
-    Particle.prototype.update = function( index ) {
-      // remove last item in coordinates array
-      this.coordinates.pop();
-      // add current coordinates to the start of the array
-      this.coordinates.unshift( [ this.x, this.y ] );
-      // slow down the particle
-      this.speed *= this.friction;
-      // apply velocity
-      this.x += Math.cos( this.angle ) * this.speed;
-      this.y += Math.sin( this.angle ) * this.speed + this.gravity;
-      // fade out the particle
-      this.alpha -= this.decay;
-      
-      // remove the particle once the alpha is low enough, based on the passed in index
-      if( this.alpha <= this.decay ) {
-        particles.splice( index, 1 );
+      update ( index ) {
+        // remove last item in coordinates array
+        this.coordinates.pop();
+        // add current coordinates to the start of the array
+        this.coordinates.unshift( [ this.x, this.y ] );
+        // slow down the particle
+        this.speed *= this.friction;
+        // apply velocity
+        this.x += Math.cos( this.angle ) * this.speed;
+        this.y += Math.sin( this.angle ) * this.speed + this.gravity;
+        // fade out the particle
+        this.alpha -= this.decay;
+        
+        // remove the particle once the alpha is low enough, based on the passed in index
+        if( this.alpha <= this.decay ) {
+          particles.splice( index, 1 );
+        }
+      }
+      draw () {
+        ctx. beginPath();
+        // move to the last tracked coordinates in the set, then draw a line to the current x and y
+        ctx.moveTo( this.coordinates[ this.coordinates.length - 1 ][ 0 ], this.coordinates[ this.coordinates.length - 1 ][ 1 ] );
+        ctx.lineTo( this.x, this.y );
+        ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
+        ctx.stroke();
       }
     }
-
-    // draw particle
-    Particle.prototype.draw = function() {
-      ctx. beginPath();
-      // move to the last tracked coordinates in the set, then draw a line to the current x and y
-      ctx.moveTo( this.coordinates[ this.coordinates.length - 1 ][ 0 ], this.coordinates[ this.coordinates.length - 1 ][ 1 ] );
-      ctx.lineTo( this.x, this.y );
-      ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
-      ctx.stroke();
-    }
-
     // create particle group/explosion
     function createParticles( x, y ) {
       // increase the particle count for a bigger explosion, beware of the canvas performance hit with the increased particles though
@@ -291,9 +285,6 @@ export default {
       e.preventDefault();
       mousedown = false;
     });
-    canvas.addEventListener( 'touchstart', function( e ) {
-      mousedown = false;
-    });
 
     // once the window loads, we are ready for some fireworks!
     window.onload = loop;
@@ -301,11 +292,13 @@ export default {
   methods:{
     full(){
       let canvas = document.getElementById("canvas");
+      console.log(this.isfull);
+      debugger
       if (this.isfull) {
-        const exitfull = () => document.cancelFullScreen || document.webkitCancelFullScreen || 
+        const exitfull = document.cancelFullScreen || document.webkitCancelFullScreen || 
           document.mozCancelFullScreen || document.exitFullScreen;
         this.isfull = false;
-        exitfull.call(window.document);
+        exitfull.call(document);
       } else {
         const fullscreen = canvas.requestFullScreen || canvas.webkitRequestFullScreen || 
           canvas.mozRequestFullScreen || canvas.msRequestFullScreen;
